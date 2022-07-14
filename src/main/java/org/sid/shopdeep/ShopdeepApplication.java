0cd17edccb97398b1db0f377a15f817b815b80d0
Ghizlane <ghizlane.elatmani.99@gmail.com>
2022-07-14 10:16:22 +0100

    first commit

---
+++ src/main/java/org/sid/shopdeep/ShopdeepApplication.java
@@ -0,0 +1,13 @@
+package org.sid.shopdeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopdeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopdeepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sid/shopdeep/ShopdeepApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.shopdeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopdeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
